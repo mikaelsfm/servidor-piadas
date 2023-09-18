6d064f6a6a6705bddb29c0294d40e9d5f6e4a27e
mikaelsfm <mikael.mesquita@gmail.com>
2023-09-18 14:42:58 -0300

    limitado a pessoas a acessarem o teatro

--- Server.py
+++ Server.py
@@ -2,14 +2,14 @@ import socket
 import threading
 import time
 
-def theater_joke(conn):
+
+def theater_joke(conn, seat_count):
     # Bilheteria
-    global seat_count
-    seat_count += 1
     time.sleep(2)
     conn.send(b"Voce entrou no teatro! Sente-se que a piada vai comecar\n")
-    time.sleep(5)
-        
+    time.sleep(3)
+    
+    print(f"seat count 3: {seat_count}")
     # Piada de Knock Knock
     conn.send(b"Knock knock!\n")
     joke_response = conn.recv(1024)
@@ -22,35 +22,43 @@ def theater_joke(conn):
             conn.send(b"R2-D2!\n")
             time.sleep(2)
             conn.send(b"Obrigado e volte sempre!")
-                
-        seat_count -= 1
+        print(f"seat count 4 {seat_count}")
+        seat_count = seat_count - 1
+        print(f"seat count 5 {seat_count}")
         conn.close()
 
-def main():
+def main(seat_count, max_seat):
     # Configurando o servidor
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("127.0.0.1", 8085))
     server.listen()
     
-    global seat_count
-    max_seat = 2
-    
-    
+    print(f"seat count 1: {seat_count}")
     while True:
-        
         conn, addr = server.accept()
-        # print(f"Conectado a {addr}")
-        
+        print(f"Conectado a {addr}")
+
         conn.send(b"Gostaria de ouvir nosso show de uma piada so?\n")
         box_response = conn.recv(1024)
-    
+
         if box_response.strip().lower() == b"sim":
             conn.send(b"Por favor aguarde que logo sera chamado\n")
-        
+
+            print(f"assentos ocupados: {seat_count}; maximo de assentos: {max_seat}")
             if seat_count <= max_seat:
+                seat_count = seat_count + 1
                 # Iniciando uma thread para lidar com o cliente
-                client_handler = threading.Thread(target=theater_joke, args=(conn,))
+                print(f"seat count 2: {seat_count}")
+                client_handler = threading.Thread(target=theater_joke, args=(conn, seat_count))
                 client_handler.start()
-                                
-seat_count = 0
-main()
+            
+            else:
+                while seat_count <= max_seat:
+                    client_handler = threading.Thread(target=theater_joke, args=(conn, seat_count))
+                    client_handler.start()
+
+
+if __name__ == '__main__':
+    max_seat = 2
+    seat_count = 0
+    main(seat_count, max_seat)
